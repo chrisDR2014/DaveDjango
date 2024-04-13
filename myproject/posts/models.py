a62810758b7afeee478ff1b09791fed2ca7d2bdf
Christoffer Carlsson <kricke87@gmail.com>
2024-04-13 13:01:11 -0400

    Lesson 3

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
